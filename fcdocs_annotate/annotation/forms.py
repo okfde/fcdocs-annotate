e4eab417f8829c3dda27ca58240a88134f1a2ca2
Magdalena Noffke <magdalena.noffke@googlemail.com>
2022-04-01 16:43:30 +0200

    Improve form

--- fcdocs_annotate/annotation/forms.py
+++ fcdocs_annotate/annotation/forms.py
@@ -10,4 +10,12 @@ class FeatureForm(forms.Form):
         for feature in Feature.objects.all():
             key = "feature_{}".format(str(feature.id))
             label = feature.question
-            self.fields[key] = forms.BooleanField(label=label, required=False)
+            help = feature.description
+            options = (('true', 'Ja'), ('false', 'Nein'))
+            self.fields[key] = forms.ChoiceField(
+                choices=options,
+                widget=forms.RadioSelect(attrs={'class': "list-unstyled"}),
+                required=True,
+                label=label,
+                help_text=help
+            )
