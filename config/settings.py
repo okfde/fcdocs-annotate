e9d46173cae2ad8e87945064b27c4f4395e6b0c2
Magdalena Noffke <magdalena.noffke@googlemail.com>
2022-09-15 16:47:28 +0200

    Remove celery settings

--- config/settings.py
+++ config/settings.py
@@ -144,11 +144,6 @@ FILINGCABINET_DOCUMENTCOLLECTION_MODEL = "filingcabinet.DocumentCollection"
 FILINGCABINET_MEDIA_PUBLIC_PREFIX = "docs"
 FILINGCABINET_MEDIA_PRIVATE_PREFIX = "docs-private"
 
-CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
-CELERY_TASK_ALWAYS_EAGER = True
-
-CELERY_TASK_ROUTES = {"filingcabinet.tasks.*": {"queue": "document"}}
-CELERY_TIMEZONE = "UTC"
 TESSERACT_DATA_PATH = values.Value("/usr/local/share/tessdata")
 
 FCDOCS_ANNOTATE_PUBLISH_DOCUMENTS = True
