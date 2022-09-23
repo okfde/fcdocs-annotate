9ce03e45cf4be7da3b6d73a8c8220a1e7bbff968
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2022-09-23 20:27:16 +0200

    Move prediction to own module, make dependencies import-optional

---
+++ fcdocs_annotate/annotation/prediction.py
@@ -0,0 +1,41 @@
+import os
+import zipfile
+from contextlib import contextmanager
+
+import pandas as pd
+from fcdocs.extras.datasets.document_dataset import DocumentDataSet
+from fcdocs.pipelines.classifier.model_dataset import ModelDataSet
+from kedro.io import Version
+
+from .models import TYPE_AUTOMATED, FeatureAnnotation
+
+
+def create_feature_annotations(feature, documents):
+    with get_prediction_model(feature.model_path) as model:
+        for doc in documents:
+            prediction = run_classification(model, doc.pdf_file.path)
+            FeatureAnnotation.objects.update_or_create(
+                defaults={"document": doc, "feature": feature, "type": TYPE_AUTOMATED},
+                value=True if prediction else False,
+            )
+
+
+def run_classification(model, pdf_file_path):
+    document = DocumentDataSet(pdf_file_path).load()
+    return model.predict(pd.DataFrame([document]))[0]
+
+
+@contextmanager
+def get_prediction_model(model_path):
+    if model_path.endswith(".zip"):
+        # Unzip model if necessary
+        real_model_path = model_path.replace(".zip", "/")
+        if not os.path.exists(real_model_path):
+            os.makedirs(real_model_path)
+            with zipfile.ZipFile(model_path, "r") as zip_ref:
+                zip_ref.extractall(real_model_path)
+        model_path = real_model_path
+
+    version = Version(None, None)
+    model = ModelDataSet(model_path, version).load()
+    yield model
--- fcdocs_annotate/annotation/tasks.py
+++ fcdocs_annotate/annotation/tasks.py
@@ -1,17 +1,53 @@
+import logging
+import tempfile
+
+import requests
 from celery import shared_task
 from fcdocs_annotate.annotation.models import Feature
 from filingcabinet import get_document_model
 
 Document = get_document_model()
+logger = logging.getLogger(__file__)
 
 
 @shared_task
 def predict_feature_for_documents(feature_id, document_ids):
     try:
+        from .prediction import create_feature_annotations
+    except ImportError:
+        logger.warn("Prediction not run, as dependencies not installed")
+        return None
+
+    try:
         feature = Feature.objects.get(id=feature_id)
-        documents = Document.objects.filter(id__in=document_ids)
-        if documents:
-            return feature.predict(documents)
-        return False
     except Feature.DoesNotExist:
-        return False
+        return
+    documents = Document.objects.filter(id__in=document_ids)
+    create_feature_annotations(feature, documents)
+
+
+@shared_task
+def predict_feature_for_document_url(feature_id, document_url, callback_url):
+    """
+    Download file to temporary file with requests, predict feature
+    and send result back to webhook callback URL.
+    """
+    try:
+        from .prediction import get_prediction_model, run_classification
+    except ImportError:
+        logger.warn("Prediction not run, as dependencies not installed")
+        return None
+
+    try:
+        feature = Feature.objects.get(id=feature_id)
+    except Feature.DoesNotExist:
+        return
+
+    with get_prediction_model(feature.model_path) as model:
+        with tempfile.NamedTemporaryFile() as tmp_file:
+            response = requests.get(document_url, stream=True)
+            for chunk in response.iter_content(chunk_size=128):
+                tmp_file.write(chunk)
+            tmp_file.flush()
+            result = run_classification(model, tmp_file.name)
+            requests.post(callback_url, json={"result": result})
