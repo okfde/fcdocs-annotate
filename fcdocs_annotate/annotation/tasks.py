bcfca04b66f0048d35039f3310ebe067423271d1
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2022-09-26 12:37:46 +0200

    Extract prediction module loading into function

--- fcdocs_annotate/annotation/tasks.py
+++ fcdocs_annotate/annotation/tasks.py
@@ -10,20 +10,26 @@ Document = get_document_model()
 logger = logging.getLogger(__file__)
 
 
+def get_prediction_module():
+    try:
+        from . import prediction
+    except ImportError as e:
+        logger.warn("Prediction not run, as dependencies not installed: %s", e)
+        return None
+    return prediction
+
+
 @shared_task
 def predict_feature_for_documents(feature_id, document_ids):
-    try:
-        from .prediction import create_feature_annotations
-    except ImportError:
-        logger.warn("Prediction not run, as dependencies not installed")
-        return None
-
+    prediction = get_prediction_module()
+    if prediction is None:
+        return
     try:
         feature = Feature.objects.get(id=feature_id)
     except Feature.DoesNotExist:
         return
     documents = Document.objects.filter(id__in=document_ids)
-    create_feature_annotations(feature, documents)
+    prediction.create_feature_annotations(feature, documents)
 
 
 @shared_task
@@ -32,22 +38,19 @@ def predict_feature_for_document_url(feature_id, document_url, callback_url):
     Download file to temporary file with requests, predict feature
     and send result back to webhook callback URL.
     """
-    try:
-        from .prediction import get_prediction_model, run_classification
-    except ImportError:
-        logger.warn("Prediction not run, as dependencies not installed")
-        return None
-
+    prediction = get_prediction_module()
+    if prediction is None:
+        return
     try:
         feature = Feature.objects.get(id=feature_id)
     except Feature.DoesNotExist:
         return
 
-    with get_prediction_model(feature.model_path) as model:
+    with prediction.get_prediction_model(feature.model_path) as model:
         with tempfile.NamedTemporaryFile() as tmp_file:
             response = requests.get(document_url, stream=True)
             for chunk in response.iter_content(chunk_size=128):
                 tmp_file.write(chunk)
             tmp_file.flush()
-            result = run_classification(model, tmp_file.name)
+            result = prediction.run_classification(model, tmp_file.name)
             requests.post(callback_url, json={"result": result})
